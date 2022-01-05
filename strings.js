f632d54106eee400708e951b8cc21a1f626c3a60
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-05 11:04:38 -0500

    generateDocument update

--- strings.js
+++ strings.js
@@ -14,7 +14,8 @@ function isPalindromeOptimal(string) {
 }
 /*
 function below operates in O(N) time since we have to check each letter in the palindrome
-operates in O(N) space since we have the call stack growing through each recursive case. really its 1/2 * N since we only call it half the times but over time constant goes away
+operates in O(N) space since we have the call stack growing through each recursive case.
+really its 1/2 * N since we only call it half the times but over time constant goes away
 */
 function isPalindromeRecursive(string, left = 0, right = string.length -1) {
   //base case
@@ -28,8 +29,10 @@ function isPalindromeRecursive(string, left = 0, right = string.length -1) {
   return isPalindromeRecursive(string, left, right);
 }
 /*
-Given a non empty string of lowercase letters and a non negative integer representing a key, write a function taht returns a new string by shifting every letter in the input string by k positions in the alphabet where k is the key.
-CHAR CODE AT is used after being given a array/string "string.charCodeAt" and it takes in the index parameter of that input and spits out
+Given a non empty string of lowercase letters and a non negative integer representing a key, write a function
+taht returns a new string by shifting every letter in the input string by k positions in the alphabet where k is the key.
+CHAR CODE AT is used after being given a array/string "string.charCodeAt" and it takes in the index parameter
+of that input and spits out
 the unicode code
 STRING. FROM CHAR CODE takes in the number code and spits out character. needs "String." to precede it
 takes O(N) time and O(N) space
@@ -50,10 +53,14 @@ function caesarCipherEncryptor(string, key){
   return newLetters.join("");
 }
 /*
-never do string += since strings concatenation is immutable, each time you do that the program needs to re read the entire string so each time its an O(N) operation!!!!!
-for space it will be 2 x n since for each character we will also have a number so that is best case scenario. sometimes you will have duplicates of each char if the count is more than 9. this leads to an O(N) operation
+never do string += since strings concatenation is immutable, each time you do that the program needs to re read the entire string so
+each time its an O(N) operation!!!!!
+for space it will be 2 x n since for each character we will also have a number so that is best case scenario.
+sometimes you will have duplicates of each char if the count is more than 9. this leads to an O(N) operation
 
-Note that I had a while loop inside a for loop which caused an O(N^2) time operation. I ended up changing it to an if statement which held the opposite logic (to not continue counting length) which made it easier to enable an O(N) operation, "string[i] !== string[i-1] || length ==9 vs while (string[i] === string[i-1] && length < 9)
+Note that I had a while loop inside a for loop which caused an O(N^2) time operation. I ended up changing it to an if statement
+which held the opposite logic (to not continue counting length) which made it easier to enable an O(N) operation,
+"string[i] !== string[i-1] || length ==9 vs while (string[i] === string[i-1] && length < 9)
 */
 
 function runLengthEncoding(string) {
@@ -74,11 +81,15 @@ function runLengthEncoding(string) {
 }
 
 /*
-Many ways to do this problem. If you loop through the document, you would need to loop through the document again to know the count of that one letter and then loop through the given characters to see if there is enough. m = length of document, n = length of characters. It would be O(m (m + n)) => m^2 + mn and then you would have o(1) for space since nothing is being created except for a count variable.
+Many ways to do this problem. If you loop through the document, you would need to loop through the document again to know the count
+of that one letter and then loop through the given characters to see if there is enough. m = length of document, n = length of characters.
+It would be O(m (m + n)) => m^2 + mn and then you would have o(1) for space since nothing is being created except for a count variable.
 
-Another solution is through memoization. You would loop through the document again and characters again and if there is enough characters for that document char then you would add that letter to the memoization object. So if you see that latter again in your loop through the document you would just continue to the next character. This would be O ( c (n + m)) with c being the number of unique characters in the document, m = length of document and and = length of characters.
+Another solution is through memoization. You would loop through the document again and characters again and if there is enough
+characters for that document char then you would add that letter to the memoization object. So if you see that latter again in your loop
+ through the document you would just continue to the next character. This would be O ( c (n + m)) with c being the number of unique characters in the document, m = length of document and and = length of characters.
 
-Best solution is to make a hash table.
+Best solution is to make a hash table. Time would be O(n + m) since you would have to loop through both strings no matter what. Space would be O(c) where c = unique chars in the character string.
 */
 
 function generateDocument(characters, document){
