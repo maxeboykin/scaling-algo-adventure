d80dda1aba342aeda4cc81176fe14c3cb6a394ad
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-05 23:10:19 -0500

    linkedlistUpdate

--- linkedLists.js
+++ linkedLists.js
@@ -1,4 +1,12 @@
+/*
+First approach can be to sort through linked list and have a memoization object storing values already collected and if a next Node has a
+repetitive value then have a while loop sorting through until the next node is distinct and make current node . next value to that node and keep going. This would be O(N) space.
 
+The answer below runs at O(N) time due to having to go through
+the entire linkedlist and then at O(1) space. Just resetting .next
+values.
+Maybe discuss the two whiles aspect.
+*/
 
 class LinkedList {
   constructor(value) {
