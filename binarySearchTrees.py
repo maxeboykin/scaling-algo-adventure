4c8878159303c2f90d00c06b888450a1fc1d97c7
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-06 11:53:27 -0500

    BSTPython

---
+++ binarySearchTrees.py
@@ -0,0 +1,30 @@
+"""
+In a BST, a nodse value must be greater than the nodes to the left
+and less than or equal to the nodes to the right. All nodes should
+satisfy this property.
+Average case for time is O(logn) and O(logn) for space if the BST is
+balanced. We will traverse on average half of the tree each time.
+Worst case is O(N) for both time and space if the tree is extremely
+unbalanced (either always left or always right node).
+"""
+
+
+class BST:
+  def __init__(self, value):
+    self.value = value
+    self.left = null
+    self.right = null
+
+
+
+def findClosestValueInBstHelper(tree, target, closest = tree.value):
+  if tree is None:
+    return closest
+  if abs(tree.value - target) < abs(closest - target):
+    closest = tree.value
+  if target < tree.value:
+    return findClosestValueInBstHelper(tree.left, target, closest)
+  elif target > tree.value:
+    return findClosestValueInBstHelper(tree.right, target, closest)
+  else:
+    return closest
