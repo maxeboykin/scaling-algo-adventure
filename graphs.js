b3d6df71ad11f94b73ba1e6c96c9fd5a17f5f877
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-15 23:10:27 -0500

    removeIslandsComments

--- graphs.js
+++ graphs.js
@@ -155,6 +155,12 @@ function traverseNodes(node, edges, colors){
 }
 
 
+/*
+Comments for removeIslands
+
+*/
+
+
 function removeIslands(matrix) {
 
 	const visitedOnes = matrix.map(row => row.map(value => false));
