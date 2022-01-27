fe666491cdd34d536c30040217aea1ff9d1c0f89
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-26 21:53:32 -0500

    numberofwaystomakechange

--- dynamicProgramming.js
+++ dynamicProgramming.js
@@ -8,3 +8,17 @@ function maxSubsetSumNoAdjacent(array) {
 	}
 	return maxSums[maxSums.length -1];
 }
+
+
+function numberOfWaysToMakeChange(n, denoms) {
+  let ways = new Array(n +1).fill(0);
+	ways[0] = 1; // at 0 there is one way of getting it ... not doing anything
+	for(let denom of  denoms){
+		for(let amount = 1; amount < n+1; amount++){
+			if(denom <= amount){
+				ways[amount] += ways[amount - denom]
+			}
+		}
+	}
+	return ways[n];
+}
