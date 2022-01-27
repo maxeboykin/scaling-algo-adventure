49fbf25a1f411cacc92184078e9f1630ce2e9b26
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-26 21:54:06 -0500

    minnumberofcoinsforchange

--- dynamicProgramming.js
+++ dynamicProgramming.js
@@ -22,3 +22,17 @@ function numberOfWaysToMakeChange(n, denoms) {
 	}
 	return ways[n];
 }
+
+
+function minNumberOfCoinsForChange(n, denoms) {
+  let numOfCoins = new Array(n + 1).fill(Infinity);
+	numOfCoins[0] = 0;
+	for(let denom of denoms){
+		for(let amount = 0; amount < numOfCoins.length; amount++){
+			if(denom <= amount){
+				numOfCoins[amount] = Math.min(numOfCoins[amount], numOfCoins[amount - denom] + 1)
+			}
+		}
+	}
+	return numOfCoins[n] !== Infinity ? numOfCoins[n] : -1;
+}
