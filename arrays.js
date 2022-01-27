e4e774757d3e149c04cc45a34719766ecd490b4f
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-26 21:48:46 -0500

    tournamentwinner

--- arrays.js
+++ arrays.js
@@ -1,3 +1,33 @@
+function tournamentWinner(competitions, results) {
+	let dict = {};
+	let currentBestTeam = '';
+	dict[currentBestTeam] = 0;
+  for(let i = 0; i < competitions.length; i++){
+		let game = competitions[i];
+		let whoLost = results[i];
+		let whoWon = 1- whoLost;
+		let winner =  game[whoWon];
+		let loser = game[whoLost];
+		if(dict[winner]){
+			dict[winner] = dict[winner] + 3;
+			if(dict[winner] >= dict[currentBestTeam]) {
+				currentBestTeam = winner;
+				dict[currentBestTeam] = dict[winner];
+			}
+		} else {
+			dict[winner] = 3;
+			if(dict[winner] >= dict[currentBestTeam]) {
+				currentBestTeam = winner;
+				dict[currentBestTeam] = dict[winner];
+			}
+		}
+		if(!dict[loser]) dict[loser] = 0;
+	}
+
+  return currentBestTeam;
+}
+
+
 function smallestDifference(arrayOne, arrayTwo) {
   arrayOne.sort((a, b) => a - b);
 	arrayTwo.sort((a, b) => a - b);
